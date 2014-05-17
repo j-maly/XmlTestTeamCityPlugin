b35c895384d0a63acdf3adb80a110a8c1503db4a
Jakub Malý <jakub@maly.cz>
2014-05-17 21:09:39 +0200

    Added another sample test

---
+++ XmlTest-agent/src/main/java/cz/jakubmaly/xmltest/agent/XmlTestBuildProcess.java
@@ -0,0 +1,7 @@
+package cz.jakubmaly.xmltest.agent;
+
+/**
+ * Created by Jakub on 11. 5. 2014.
+ */
+public class XmlTestBuildProcess {
+}
---
+++ XmlTest-agent/src/main/java/cz/jakubmaly/xmltest/agent/XmlTestMultiCommandBuildSession.java
@@ -0,0 +1,7 @@
+package cz.jakubmaly.xmltest.agent;
+
+/**
+ * Created by Jakub on 17. 5. 2014.
+ */
+public class XmlTestMultiCommandBuildSession {
+}
